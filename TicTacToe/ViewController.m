614bed3056c8a0f62240a29f4bb02b9dcac0c0d8
Rosemary Zalewski <motleyrosemary@gmail.comm>
2015-05-14 14:57:55 -0500

    <Built panel, added 9 buttons and related actions, added which player label>

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+@property (weak, nonatomic) IBOutlet UILabel *labelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *labelThree;
+@property (weak, nonatomic) IBOutlet UILabel *labelFour;
+@property (weak, nonatomic) IBOutlet UILabel *labelFive;
+@property (weak, nonatomic) IBOutlet UILabel *labelSix;
+@property (weak, nonatomic) IBOutlet UILabel *labelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *labelEight;
+@property (weak, nonatomic) IBOutlet UILabel *labelNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property CGPoint tappedPoint;
 
 @end
 
@@ -19,9 +30,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)findLabelUsingPoint:(CGPoint)point
+
+- (IBAction)onLabelTapped:(id)sender {
 }
+
 
 @end
